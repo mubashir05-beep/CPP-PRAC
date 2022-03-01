5774ce23d49524151176ba320e02510939317d29
Mubashir <mubashir.munir2020@gmail.com>
2022-03-01 22:36:28 +0500

    .vscode/launch.json .vscode/tasks.json classes classes.cpp

---
+++ classes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class syntex
+{
+    public:
+    int age=18;
+    string name="Mubashir";
+};
+int main()
+{
+    syntex S;
+    cout<<"My name is "<<S.name<<endl;
+    cout<<"My name Is "<<S.age<<endl;
+    return 0;
+
+}
