289dbca317e09f8a41a9f6868117864910428bc8
Mubashir <mubashir.munir2020@gmail.com>
2022-03-02 19:05:51 +0500

    Edit classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -17,11 +17,11 @@ int main()
 {
     syntex S;
     cout<<"My name is "<<S.name<<endl;
-    cout<<"My name Is "<<S.age<<endl;
+    cout<<"My name Is "<<S.age<<endl ;
     sum s;
     cout<<"Enter 2 Numbers:"<<endl;
     cin>>s.a>>s.b;
-    cout<<"Addition ="<<int sum::add(s.a,s.b);
+    cout<<"Addition ="<<add(s.a,s.b);
     return 0;
 }
 int add(int a, int b)
