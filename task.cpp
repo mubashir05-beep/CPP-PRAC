ffd2ca97d269c66fe621a47998a77daeff257c89
Mubashir <mubashir.munir2020@gmail.com>
2022-03-24 21:49:13 +0500

    add

--- task.cpp
+++ task.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 class Employee
 {
@@ -21,4 +21,25 @@ int main()
   Employee e1,e2,e3,e4;
   cout<<"Total Number of Employes are : "<<Employee::id<<endl;
   cout<<"-------------------------"<<endl;
+}*/
+#include <iostream>
+using namespace std;
+class library{
+    public:
+    static int book_id;
+    char book[10];
+    library(){
+    book_id++;
+    cout<<"Please Enter Book name: "<<endl;
+    cin>>book;
+    cout<<"Your Book ID is : "<<book_id<<endl;
+   
+    }
+};
+int main()
+{
+    library b1,b2,b3,b4;
+     cout<<"The Total Books You Borrowed : "<<library::book_id<<endl;
+
+
 }
