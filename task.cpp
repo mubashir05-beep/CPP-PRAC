0d8769a75a266e18b0babc2b588c078f61480091
Mubashir <mubashir.munir2020@gmail.com>
2022-04-03 10:45:42 +0500

    Add

--- task.cpp
+++ task.cpp
@@ -22,7 +22,7 @@ int main()
   cout<<"Total Number of Employes are : "<<Employee::id<<endl;
   cout<<"-------------------------"<<endl;
 }*/
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 class library{
     public:
@@ -40,6 +40,57 @@ int main()
 {
     library b1,b2,b3,b4;
      cout<<"The Total Books You Borrowed : "<<library::book_id<<endl;
-
-
+}
+#include<iostream>
+using namespace std;
+class bus
+{
+    public:
+    static int seat_id;
+    string name;
+    string location_name;
+    bus(){
+cout<<"Enter Your name:"<<endl;
+cin>>name;
+cout<<"Enter Your location your going to :"<<endl;
+cin>>location_name;
+ seat_id++;
+cout<<"Your Seast ID is "<<seat_id;
+    }
+};
+int bus::seat_id=0;
+int main()
+{
+    bus s1,s2,s3;
+}
+*/
+#include<iostream>
+using namespace std;
+class area{
+    public:
+static  int a;
+static int b;
+static int c;
+area(){
+  //a++;
+    b=b+2;
+    c=c+3;
+    cout<<"Runnning"<<endl;
+}
+// int a,int b, int c
+static void display(){
+    cout<<a<<b<<c<<endl;
+}
+static int sum(int a , int b , int c){
+    return a+b+c;
+}
+};
+int area::a=0;
+int area::b=0;
+int area::c=0;
+int main(){
+    area ob1;
+   ob1.display();
+   cout<<"Sum"<<area::sum(area::a,area::b,area::c);
+//    cout<<ob1.sum(area::a,area::b,area::c);
 }
